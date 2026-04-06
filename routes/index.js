9c7d8f9288ab6cd8b546142e9be44ec6921270db
Brice Norton Hennelly <bricenorton@fas.harvard.edu>
2026-04-06 14:58:59 -0400

    update index to use new mealService

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,18 @@
-var express = require('express');
-var router = express.Router();
-const Meal = require('../models/mealModel');
+const express = require('express');
+const router = express.Router();
+// const Meal = require('../models/mealModel');
+
+const mealService = require('../services/mealService');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   try {
     // find all the meals in the database
     // future enhancment will only find the meals that match with Today.
-    const meals = await Meal.find();
+
+    // const meals = await Meal.find();
+
+    const meals = await mealService.list();
     res.render('index', {
       title: 'What Did I Eat Today?',
       meals: meals.map((meal) => ({
